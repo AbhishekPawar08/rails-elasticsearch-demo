608f7976e5c1ccf8cd3b0d283b4808e5627dfeb4
Pedro Alonso <pedro@pedroalonso.net>
2016-03-22 16:41:11 +0100

    search highlight moved to searchkick

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,27 +1,3 @@
-require 'elasticsearch/model'
-
 class Article < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Elasticsearch::Model::Callbacks
-
-  def self.search(query)
-    __elasticsearch__.search(
-      {
-        query: {
-          multi_match: {
-            query: query,
-            fields: ['title', 'text']
-          }
-        },
-        highlight: {
-          pre_tags: ['<em>'],
-          post_tags: ['</em>'],
-          fields: {
-            title: {},
-            text: {}
-          }
-        }
-      }
-    )
-  end
+  searchkick
 end
